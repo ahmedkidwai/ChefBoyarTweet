ac46801f88807b1772d64dc1c149be9662a057b7
Ahmed <ahmedkiwai@icloud.com>
2021-01-24 05:58:05 -0600

    Implemented Recipe Image Method

--- ChefBoyarTweetBot.py
+++ ChefBoyarTweetBot.py
@@ -4,6 +4,9 @@ from recipe_scrapers import scrape_me
 from RevImg import RevImg
 from textblob import TextBlob
 from bs4 import BeautifulSoup
+from PIL import Image, ImageFont, ImageDraw
+import textwrap
+
 
 def bestImageGuess(imageLink):
     ri = RevImg()
@@ -19,9 +22,47 @@ def removeNouns(best_guess):
     nouns = [word for (word, pos) in nltk.pos_tag(tokenized) if is_noun(pos)] 
     return (' '.join(nouns)) 
     
-def get_recipe(food):
+def get_recipe(recipe_url):
     
-    scraper = scrape_me('https://www.allrecipes.com/recipe/158968/spinach-and-feta-turkey-burgers/')
+    scraper = scrape_me(recipe_url)
+    print(type(scraper.title()))
+    print(type(scraper.instructions()))
+    
+    myIngredients = scraper.ingredients()
+    myIngredientsString = ', '.join(myIngredients[1:])
+    print(myIngredientsString)
+    # creating a image object  
+    image = Image.open(r'test.jpg')  
+    
+    draw = ImageDraw.Draw(image)  
+    
+    # specified font size 
+    
+    image = Image.open(r'test.jpg') 
+    fontsize = 20  # starting font size
+    font = ImageFont.truetype('gillsans.ttf', fontsize) 
+    text1 = myIngredientsString
+
+    text_color = (0, 0, 0)
+    text_start_height = 5
+    draw_multiple_line_text(image, scraper.title(), font, 'red', text_start_height)
+    draw_multiple_line_text(image, "Ingredients", font, 'red', 50)
+    draw_multiple_line_text(image, text1, font, text_color, 75)
+    draw_multiple_line_text(image, "Instructions", font, 'red', 575)
+    draw_multiple_line_text(image, scraper.instructions(), font, text_color, 600)
+    image.save('pil_text.png')
+
+def draw_multiple_line_text(image, text, font, text_color, text_start_height):
+   
+    draw = ImageDraw.Draw(image)
+    image_width, image_height = image.size
+    y_text = text_start_height
+    lines = textwrap.wrap(text, width=40)
+    for line in lines:
+        line_width, line_height = font.getsize(line)
+        draw.text(((image_width - line_width) / 2, y_text), 
+                  line, font=font, fill=text_color)
+        y_text += line_height
 
 def find_recipe(food):
     
@@ -32,17 +73,16 @@ def find_recipe(food):
     parsedText = soup.prettify()
     startOne = parsedText.find('o-ResultCard__m-MediaBlock m-MediaBlock')
     parsedUpate = parsedText[startOne:]
-    #print(parsedUpate)
     startTwo = parsedUpate.find('www')
     parsedUpdate2 = parsedUpate[startTwo:]
     end = parsedUpdate2.find('"')
-    print(parsedUpdate2[:end])
+    return ("https://" + parsedUpdate2[:end])
 
 def main():
     best_guess = bestImageGuess("nope")
     food = removeNouns(best_guess)
     recipe_url = find_recipe(food)
-    # get_recipe(food)
+    get_recipe(recipe_url)
 
 if __name__ == "__main__":
     main()

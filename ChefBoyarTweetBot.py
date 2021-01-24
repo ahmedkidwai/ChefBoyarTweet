06a3bcd75ee2ea7d0b791a38a48e9a160b4d887c
Ahmed <ahmedkiwai@icloud.com>
2021-01-24 04:31:36 -0600

    Created Initial Bot + Methods to Find Image, remove nouns, and find recipe

---
+++ ChefBoyarTweetBot.py
@@ -0,0 +1,44 @@
+import nltk
+import requests
+from recipe_scrapers import scrape_me
+from RevImg import RevImg
+from textblob import TextBlob
+from bs4 import BeautifulSoup
+
+def bestImageGuess(imageLink):
+    ri = RevImg()
+    best_guess = ri.get_best_guess("https://gimmedelicious.com/wp-content/uploads/2014/03/Cauliflower-Crust-Pizza-1-500x500.jpg")
+    return best_guess
+
+def removeNouns(best_guess):
+    lines = 'lines is some string of words'
+    # function to test if something is a noun
+    is_noun = lambda pos: pos[:2] == 'NN'
+    # do the nlp stuff
+    tokenized = nltk.word_tokenize(best_guess)
+    nouns = [word for (word, pos) in nltk.pos_tag(tokenized) if is_noun(pos)] 
+    return (' '.join(nouns)) 
+    
+def get_recipe(food):
+    
+    scraper = scrape_me('https://www.allrecipes.com/recipe/158968/spinach-and-feta-turkey-burgers/')
+
+def find_recipe(food):
+    
+    myString = "https://www.foodnetwork.com/search/" + food.replace(" ","-") + "-/CUSTOM_FACET:RECIPE_FACET"
+    print(myString)
+    URL = myString
+    r = requests.get(URL) 
+    soup = BeautifulSoup(r.content, 'html5lib') # If this line causes an error, run 'pip install html5lib' or install html5lib 
+    parsedText = soup.prettify()
+    start = parsedText.find('o-ResultCard__m-MediaBlock m-MediaBlock')
+    print(parsedText[start:])
+
+def main():
+    best_guess = bestImageGuess("nope")
+    food = removeNouns(best_guess)
+    recipe_url = find_recipe(food)
+    # get_recipe(food)
+
+if __name__ == "__main__":
+    main()
